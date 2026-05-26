0e497942c9959be72a73345b0361b07f0c703fb9
andrey <oleg.sudorskuy@gmail.com>
2026-05-26 17:35:36 +0200

    Solution2

--- app/main.py
+++ app/main.py
@@ -1,28 +1,29 @@
 class Car:
-    def __init__(self, comfort_class, clean_mark, brand):
+    def __init__(self, comfort_class: int, clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
-    pass
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center, clean_power,
-                 average_rating, count_of_ratings):
+    def __init__(self, distance_from_city_center: int, clean_power: int,
+                 average_rating: float, count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
         self.count_of_ratings = count_of_ratings
 
-    def calculate_washing_price(self, car):
-        return round((car.comfort_class * (self.clean_power - car.clean_mark) *
-                 self.average_rating / self.distance_from_city_center), 1)
+    def calculate_washing_price(self, car: list) -> float:
+        return round((car.comfort_class * (self.clean_power - car.clean_mark)
+                     * self.average_rating / self.distance_from_city_center),
+                     1)
 
-    def wash_single_car(self, car):
+    def wash_single_car(self, car: list) -> None:
         if self.clean_power > car.clean_mark:
             car.clean_mark = self.clean_power
 
-    def serve_cars(self, list_of_cars):
+    def serve_cars(self, list_of_cars: list) -> float:
         income = 0
         for car in list_of_cars:
             if self.clean_power > car.clean_mark:
@@ -30,7 +31,7 @@ class CarWashStation:
                 self.wash_single_car(car)
         return round(income, 1)
 
-    def rate_service(self, rate):
+    def rate_service(self, rate: float) -> float:
         sum_all_rate = self.average_rating * self.count_of_ratings
         sum_all_rate += rate
         self.count_of_ratings += 1
